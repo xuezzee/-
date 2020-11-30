9171732033bd8d3d9afcbcb3ed52f23afefbf9ab
Yutongamber <56161755+Yutongamber@users.noreply.github.com>
2020-11-30 12:17:42 +0800

    independent_2_agents

--- agents/RL_trainer.py
+++ agents/RL_trainer.py
@@ -16,6 +16,8 @@ import torch
 import os
 import numpy as np
 
+import tkinter
+
 # base_dir = Path(__file__).resolve().parent.parent
 # print(base_dir)
 # sys.path.append(str(base_dir))
@@ -33,12 +35,12 @@ from env.transport_dqn import Transport
 map_path = 'env'
 name ='Materials Transport'
 conf = {
-        'n_player': 1,#玩家数量
+        'n_player': 2,#玩家数量
         'board_width': 11,#地图宽
         'board_height': 11,#地图高
         'n_cell_type': 5,#格子的种类
         'materials': 4,#集散点数量
-        'cars': 1,#汽车数量
+        'cars': 2,#汽车数量
         'planes': 0,#飞机数量
         'barriers': 12,#固定障碍物数量
         'max_step' :2000,#最大步数
@@ -73,7 +75,7 @@ parser.add_argument("--epsilon-decay", type=float, default=0.998, help="Decay fa
 
 parser.add_argument("--episode-length", default = conf['max_step'], type = int)
 
-parser.add_argument("--test-id", default = 'test13')
+parser.add_argument("--test-id", default = 'test16')
 
 flags = parser.parse_args()
 
@@ -88,7 +90,7 @@ np.random.seed(1)
 #     pass
 
 ESOURCE_PATH = os.path.dirname(os.path.realpath(__file__))
-path = os.path.join(ESOURCE_PATH , 'RzrqResFile', flags.test_id)
+path = os.path.join(ESOURCE_PATH , flags.test_id, flags.test_id)
 if not os.path.exists(path):
     os.makedirs(path)
 
@@ -117,7 +119,7 @@ env = Transport(conf)
 
 # TODO UPDATE
 # Calculate the state size based on the number of nodes in the tree observation
-state_size = 122
+state_size = 124
 action_size = 5
 
 # Add some variables to keep track of the progress
@@ -151,16 +153,13 @@ def obs_wrapper(obss):
     '''
     return np.array(obss)
 
-def action_wrapper(action_dict):
+def state_to_obs(obss, n):
+    return np.array(obss[0:121] + obss[ (121+ 3*n) : (124+3*n)])
 
-    action = action_dict[0] # index: agent id
-    each = [0] * 5
-    each[action] = 1
-    action_one_hot = [[each]]
+def action_wrapper(action_dict):
 
     # add: 前两个action
     joint_action = []
-
     for i in range(2):
         player = []
         for j in range(1):
@@ -170,7 +169,12 @@ def action_wrapper(action_dict):
             player.append(each)
         joint_action.append(player)
 
-    joint_action.append([action_one_hot[0][0]])
+    for a in range(flags.num_agents):
+        action = action_dict[a] # index: agent id
+        each = [0] * 5
+        each[action] = 1
+        action_one_hot = [[each]]
+        joint_action.append([action_one_hot[0][0]])
 
     return joint_action
 
@@ -180,25 +184,27 @@ for episode in range(start + 1, flags.num_episodes + 1):
     # print('epi', episode)
     agent.reset()
     obs = env.reset()
+
     score, steps_taken = 0, 0
 
     # Build initial observations for each agent
     for a in range(flags.num_agents):
-        agent_obs[a] = obs_wrapper(obs)
+        # agent_obs[a] = obs_wrapper(obs)
+        agent_obs[a] = state_to_obs(obs, a)
         agent_obs_buffer[a] = agent_obs[a].copy()
 
     action_dict = {}
 
     # Run an episode
     for step in range(flags.episode_length):
-        env._render()
+        # env._render()
         steps_taken += 1
         update_values = [False] * flags.num_agents
         action_dict = {}
 
         # 对每一个agent算法进行更新
         # TODO UPDATE
-        # for a in range(flags.num_agents):
+        for a in range(flags.num_agents):
         #     if info['action_required'][a]:
         #           action_dict[a] = agent.act(agent_obs[a], eps=eps)
         #           # action_dict[a] = np.random.randint(5)
@@ -206,28 +212,24 @@ for episode in range(start + 1, flags.num_episodes + 1):
         #           steps_taken += 1
         #     else: action_dict[a] = 0
 
-        action_dict[a] = agent.act(agent_obs[0], eps = eps)
+            action_dict[a] = agent.act(agent_obs[a], eps = eps)
 
         joint_action = action_wrapper(action_dict)
 
         obs, rewards, done, info = env.step(joint_action)
-        # if rewards[0][0] != 0 or rewards[0][1] != 0:
 
-        score += rewards[0][0]
-        # if score != 0:
+        for a in range(flags.num_agents):
+            # agent_obs[a] = obs_wrapper(obs)
+            agent_obs[a] = state_to_obs(obs, a)
 
-
-        # Check for collisions and episode completion
-        # if step == max_steps - 1:
-        #     done['__all__'] = True
-
-
+        for a in range(flags.num_agents):
+            score += rewards[a]
 
         # Update replay buffer and train agent
         for a in range(flags.num_agents):
             if flags.train:  #and (update_values[0] or done or done['__all__']):
                 # push: agent_id, obs_old, action, reward, obs_new, done
-                agent.step(a, agent_obs_buffer[a], agent_action_buffer[a], rewards[0][a], agent_obs[a], [done][a])
+                agent.step(a, agent_obs_buffer[a], agent_action_buffer[a], rewards[a], agent_obs[a], [done][0]) # TODO done need to updated
                 agent_obs_buffer[a] = agent_obs[a].copy()
                 agent_action_buffer[a] = action_dict[a]
 
@@ -236,7 +238,8 @@ for episode in range(start + 1, flags.num_episodes + 1):
             # if obs[a]:
             #     print('obs[a]', obs[a])
             #     print('agent_obs[a]', agent_obs[a])
-            agent_obs[a] = obs_wrapper(obs)
+
+            # agent_obs[a] = obs_wrapper(obs)
 
         # Render
         # if flags.render_interval and episode % flags.render_interval == 0:
@@ -251,23 +254,27 @@ for episode in range(start + 1, flags.num_episodes + 1):
         # torch.save(agent.qnetwork_local.state_dict(),
         #            './' + 'test10' +  '/navigator_checkpoint'  + '.pth')
 
+        torch.save(agent.qnetwork_local.state_dict(),
+                   './' + flags.test_id + '/navigator_checkpoint.pth')
+
         # print(env.is_terminal() or score == 4 )
-
-
         if env.is_terminal() or score == 8 :
             # print(score, steps_taken)
             break
-    if eps % 50 == 0:
-        agent.save('./' + flags.test_id + '/navigator_checkpoint' + '.pth')
+
     # print(steps_taken)
     scores_window.append(score)
     steps_window.append(steps_taken)
     print('\r epi:{} , score:{}, step_taken:{}, score_avg:{:.3f} , step_avg:{:.3f}'.format(episode, score, steps_taken, np.mean(scores_window), np.mean(steps_window) ))
     # Epsilon decay
+    # if np.mean(scores_window) > 1.8:
+    #     eps = 0.01
+
     if flags.train: eps = max(0.1, flags.epsilon_decay * eps)
 
     # Save some training statistics in their respective deques
-
+    # if episode % 20 == 0:
+    #     agent.save('./' + 'RzrqResFile/'+ flags.test_id +  '/navigator_checkpoint'  + '.pth')
 
 
     # scores_window.append(score )
@@ -288,5 +295,7 @@ for episode in range(start + 1, flags.num_episodes + 1):
     summary.add_scalar('performance/steps_taken', steps_taken, episode)
 
 print('training_time:', time.time() - start_time)
+# torch.save(agent.qnetwork_local.state_dict(),
+#                    './' + flags.test_id +  '/navigator_checkpoint'  + '.pth')
 torch.save(agent.qnetwork_local.state_dict(),
-                   './' + flags.test_id +  '/navigator_checkpoint'  + '.pth')
+                   './' + flags.test_id + '/navigator_checkpoint.pth')
